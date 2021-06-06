b0f4dff6149041b7c5c9923d6822039e9d1ced5b
diasmpd <bgmateushenrique@gmail.comgit config --global user.name diasmpdgit config --global user.email bgmateushenrique@gmail.com>
2021-06-06 17:20:47 -0300

    criacao de repositorio com o codigo do bootcamp

---
+++ src/main/java/com/project/bootcamp/exceptions/BussinessException.java
@@ -0,0 +1,8 @@
+package com.project.bootcamp.exceptions;
+
+public class BussinessException extends RuntimeException {
+
+    public BussinessException(String message){
+        super(message);
+    }
+}
